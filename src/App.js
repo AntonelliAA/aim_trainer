9025e1815dad7f8862e5b367e4c67aa7e855ae89
Anthony Antonelli <anthony.anthony179@gmail.com>
2023-03-14 18:12:31 -0300

    Testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [positionX, setPositionX] = useState("");
+  const [positionY, setPositionY] = useState("");
+  const [timeLeft, setTimeLeft] = useState(10);
+  const [disabled, setDisabled] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleClick = () => {
+    setPositionX(Math.floor(Math.random() * 200));
+    setPositionY(Math.floor(Math.random() * 200));
+    console.log(positionX + "e" + positionY);
+    setScore(score + 10);
+  };
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      setDisabled(true);
+    } else {
+      const timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [timeLeft]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Pontuação: {score}</h1>
+      <h1>Tempo: {timeLeft}</h1>
+      <div id="game">
+        <div
+          id="cu"
+          style={{
+            position: "absolute",
+            top: `${positionY}px`,
+            left: `${positionX}px`,
+          }}
+          onClick={handleClick}
         >
-          Learn React
-        </a>
-      </header>
+          <button disabled={disabled}>O</button>
+        </div>
+      </div>
     </div>
   );
 }
